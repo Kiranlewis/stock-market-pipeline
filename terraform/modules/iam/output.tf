a0d0eb63d8413c1171c6d90b7ad9bb309bfa55b3
Kiranlewis <arunsaikirans@gmail.com>
2026-06-04 15:39:09 +0530

    New build terraform

--- terraform/modules/iam/output.tf
+++ terraform/modules/iam/output.tf
@@ -1,24 +1,9 @@
-output "bronze_bucket_name" {
-  description = "Bronze bucket name"
-  value       = module.s3.bronze_bucket_name
-}
-
-output "silver_bucket_name" {
-  description = "Silver bucket name"
-  value       = module.s3.silver_bucket_name
-}
-
-output "gold_bucket_name" {
-  description = "Gold bucket name"
-  value       = module.s3.gold_bucket_name
-}
-
 output "lambda_role_arn" {
   description = "Lambda IAM role ARN"
-  value       = module.iam.lambda_role_arn
+  value       = aws_iam_role.lambda_role.arn
 }
 
 output "glue_role_arn" {
   description = "Glue IAM role ARN"
-  value       = module.iam.glue_role_arn
+  value       = aws_iam_role.glue_role.arn
 }
